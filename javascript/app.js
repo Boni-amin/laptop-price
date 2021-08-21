9389624b0e3648e457e8d92c57bdbe1a1b65ba5d
Boni-amin <boniamin.info@gmail.com>
2021-08-21 22:16:01 +0600

    add new section

--- javascript/app.js
+++ javascript/app.js
@@ -3,11 +3,13 @@ function getMemoryNumber(memoryCost, number){
     const totalNumberText = document.getElementById(memoryCost);
     totalNumberText.innerText = number;
 }
+// get inner text 
 function getTotalInnerText(inputText){
     const bestNumber = document.getElementById(inputText);
     const bestNumberValue = Number(bestNumber.innerText);
     return bestNumberValue;
 }
+// Total price Function 
 function getTotalPrice(){
     const bestNumberValue = getTotalInnerText('best-number');
     const memoryNumberValue = getTotalInnerText('memory-number');
@@ -25,38 +27,54 @@ function getTotalPrice(){
 // Memory button handle 
 document.getElementById('memory-button1').addEventListener('click', function(){
     getMemoryNumber('memory-number', 0);
+    // Total price 
     getTotalPrice()
 })
 document.getElementById('memory-button2').addEventListener('click', function(){
     getMemoryNumber('memory-number', 180);
+    // Total price
     getTotalPrice();
 
 });
 // Strong button handle 
 document.getElementById('strong-button1').addEventListener('click', function(){
     getMemoryNumber('storng-number', 0);
+    // Total price
     getTotalPrice();
 });
 document.getElementById('strong-button2').addEventListener('click', function(){
     getMemoryNumber('storng-number', 100);
+    // Total price
     getTotalPrice();
 });
 document.getElementById('strong-button3').addEventListener('click', function(){
     getMemoryNumber('storng-number', 180);
+    // Total price
     getTotalPrice();
 });
 //  Delivary option  handle 
 document.getElementById('delivary-button1').addEventListener('click', function(){
     getMemoryNumber('delivary-number', 0);
+    // Total price
     getTotalPrice();
 });
 document.getElementById('delivary-button2').addEventListener('click', function(){
     getMemoryNumber('delivary-number', 20);
+    // Total price
     getTotalPrice();
 });
 
 // Apply button handle 
 document.getElementById('apply-button').addEventListener('click', function(){
-    getTotalPrice()
-   
+    const finalBalance =  document.getElementById('final-total-number');
+    const finalBalanceValue = finalBalance.innerText;
+    const cuponInput = document.getElementById('cupon-input');
+    const cuponInputValue = cuponInput.value;
+    const cupon = finalBalanceValue * 20 / 100;
+    
+    if (cuponInputValue == 'stevekaku') {
+        const totalBalance = finalBalanceValue - cupon;
+        finalBalance.innerText = totalBalance;
+    }
+    cuponInput.value = '';
 });
